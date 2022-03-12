5d234049425a1570e6d9dbe131cc622f5ef10832
1mposs1blyt <60925262+1mposs1blyt@users.noreply.github.com>
2022-03-12 21:04:47 +0700

    Add files via upload

---
+++ VueJS/example/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <test-component></test-component>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from "./components/test.vue";
+export default {
+  name: "App",
+  components: {
+    "test-component": Test,
+  },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ VueJS/example/src/components/test.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+      <h1 v-bind:class="{color: color}">hello</h1>
+      <button v-on:click="func">Click!</button>
+      <h2>{{count}}</h2>
+      <p>{{name}}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  namesurname:{
+      name:"Vasya",
+      surname:"Ivanov"
+  },
+  data(){
+      return{
+          count: 1,
+          color: false
+      }
+  },
+  methods:{
+      func (){
+          this.count++
+        //   alert("clicked!");// v-on:mouseover="func" v-on:mouseout="func"
+      },watch:{
+          count: function(){
+            this.color != this.color
+          },
+      }
+  },
+};
+</script>
+
+<style lang="less">
+.color{
+    background: rgb(255, 0, 0);
+}
+</style>
